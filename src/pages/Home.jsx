afd771131ac8614214710f3bffac0054e64a7675
XinMiaoWang <cvison.engineer.1@gmail.com>
2023-05-14 16:49:51 +0800

    Add categories and postCards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import Announcement from '../components/Announcement'
+import Feature from '../components/Feature'
 
 import './home.scss'
 
@@ -11,6 +12,7 @@ const Home = () => {
       <Navbar/>
       <Header/>
       <Announcement/>
+      <Feature/>
     </div>
   )
 }
---
+++ src/subcomponents/Categories.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+ 
+import './categories.scss';
+
+const Categories = ({dataArray}) => {
+    return (
+        <div className="catergies">
+            {
+                dataArray.map((item, index) => {
+                    return (
+                        <div className="item" key={index}>
+                            <img src={item.img} alt="" />
+                            <div className="itemInfo">
+                                <div className="title">
+                                    {item.name}
+                                </div>
+                                <div className="desc">
+                                    {item.amount}
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+}
+
+export default Categories;
