f91dba6018748fb364cd777402c1e6ae0beaeaaf
XinMiaoWang <cvison.engineer.1@gmail.com>
2023-05-21 17:17:48 +0800

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./app.scss"
 import Home from "./pages/Home"
+import HotelsList from './pages/HotelsList'
+import Hotel from './pages/Hotel'
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/hotelsList" element={<HotelsList/>} />
+        <Route path="/hotels/:id" element={<Hotel/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
